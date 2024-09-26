9af0273bd8128883ad17c9020e2eb900f1e12ee6
sunwchoi <101624921+sunwchoi@users.noreply.github.com>
2024-09-26 14:11:35 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 35.53 ms, Memory: 32.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    unordered_map<string, int> mp;
+    
+    for (int i = 0; i < participant.size(); i++)
+    {
+        mp[participant[i]]++;
+    }
+    
+    for (int i = 0; i < completion.size(); i++)
+    {
+        mp[completion[i]]--;
+    }
+    
+    for (auto it = mp.begin(); it != mp.end(); it++)
+    {
+        if (it->second > 0)
+            return it->first;
+            
+    }
+    return answer;
+}
