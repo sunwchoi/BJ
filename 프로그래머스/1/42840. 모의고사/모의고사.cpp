3b0caa591e28aa5e8c14cbd09735277d92542b9e
sunwchoi <101624921+sunwchoi@users.noreply.github.com>
2024-10-30 11:04:43 +0900

    [level 1] Title: 모의고사, Time: 0.10 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -9,6 +9,7 @@ vector<int> solution(vector<int> answers) {
     vector<int> two = {2, 1, 2, 3, 2, 4, 2, 5};
     vector<int> three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
     vector<int> ret(3);
+    vector<int> answer;
     
     for (int i = 0; i < answers.size(); i++)
     {
@@ -20,18 +21,11 @@ vector<int> solution(vector<int> answers) {
             ret[2]++;
     }
     
-    
-    if (ret[0] > ret[1] && ret[0] > ret[2])
-        return {1};
-    if (ret[1] > ret[2] && ret[1] > ret[0])
-        return {2};
-    if (ret[2] > ret[1] && ret[2] > ret[0])
-        return {3};
-    if (ret[0] == ret[1] && ret[0] > ret[2])
-        return {1, 2};
-    if (ret[0] == ret[2] && ret[0] > ret[1])
-        return {1, 3};
-    if (ret[1] == ret[2] && ret[1] > ret[0])
-        return {2, 3};
-    return {1, 2, 3};    
+    int mx = 0;
+    for (int i = 0; i < 3; i++)
+        mx = max(mx, ret[i]);
+    for (int i = 0; i < 3; i++)
+        if (mx == ret[i])
+            answer.push_back(i + 1);
+    return answer;
 }
